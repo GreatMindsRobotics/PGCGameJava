82ca61595acc963dadff5048f79282f468ab0e00
CoolBots <stan@buildcoolrobots.com>
2013-07-26 09:47:29 -0700

    test project update

---
+++ PGCGameTestCode-android/gen/com/buildcoolrobots/test/pgcgametestcode/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.buildcoolrobots.test.pgcgametestcode;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
