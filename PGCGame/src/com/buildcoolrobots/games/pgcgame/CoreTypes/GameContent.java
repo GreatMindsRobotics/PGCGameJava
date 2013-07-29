5d38f944d4d36bcdcb4d924d905e87edd3e0af39
binkers <brandon98@gmail.com>
2013-07-29 14:41:37 -0700

    loaded ship and enemy drones
    also changed name of purpshiprotatedlibgdx to just purpshiprotated

--- PGCGame/src/com/buildcoolrobots/games/pgcgame/CoreTypes/GameContent.java
+++ PGCGame/src/com/buildcoolrobots/games/pgcgame/CoreTypes/GameContent.java
@@ -1,15 +1,18 @@
 package com.buildcoolrobots.games.pgcgame.CoreTypes;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 
 public class GameContent {
-	public static class LoadContent {
+	public static class Ships {
 		Texture enemyDrone1;
 		Texture enemyDrone2;
-		Texture purpShipRotated;
+		Texture playerShip;
 		
-		static {
-			
+		Ships (){
+			playerShip = new Texture(Gdx.files.internal("Images/PurpShipRotated"));
+			enemyDrone1 = new Texture(Gdx.files.internal("Images/EnemyDrone"));
+			enemyDrone2 = new Texture(Gdx.files.internal("Images/EnemyDrone2"));
 		}
 	}
 }
