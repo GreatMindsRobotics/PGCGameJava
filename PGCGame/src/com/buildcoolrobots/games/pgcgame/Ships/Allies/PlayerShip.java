072686babc2553b1fbfaffffe0aed49a03946046
rehaanster11 <rehaanirf@yahoo.com>
2013-07-29 13:34:26 -0700

    Added Images and Enemy package in ship package
    didn't do bosses yet in enemy

---
+++ PGCGame/src/com/buildcoolrobots/games/pgcgame/Ships/Allies/PlayerShip.java
@@ -0,0 +1,7 @@
+package com.buildcoolrobots.games.pgcgame.Ships.Allies;
+
+import com.buildcoolrobots.games.pgcgame.Ships.BaseShip;
+
+public class PlayerShip extends BaseShip {
+
+}
---
+++ PGCGame/src/com/buildcoolrobots/games/pgcgame/Ships/Enemies/EnemyBattleCruiser.java
@@ -0,0 +1,5 @@
+package com.buildcoolrobots.games.pgcgame.Ships.Enemies;
+
+public class EnemyBattleCruiser {
+
+}
