60281559863b77eb5c6fde86eeb574be670f36db
binkers <brandon98@gmail.com>
2013-07-29 14:10:31 -0700

    changed ship class to load content

--- PGCGame/src/com/buildcoolrobots/games/pgcgame/CoreTypes/GameContent.java
+++ PGCGame/src/com/buildcoolrobots/games/pgcgame/CoreTypes/GameContent.java
@@ -1,7 +1,15 @@
 package com.buildcoolrobots.games.pgcgame.CoreTypes;
 
+import com.badlogic.gdx.graphics.Texture;
+
 public class GameContent {
-	public static class Ships {
+	public static class LoadContent {
+		Texture enemyDrone1;
+		Texture enemyDrone2;
+		Texture purpShipRotated;
 		
+		static {
+			
+		}
 	}
 }
