493ef4f08972d3a79b029be008e1068c45446e69
rehaanster11 <rehaanirf@yahoo.com>
2013-07-29 14:10:17 -0700

    Added Bullet Package and Powerups Package
    bullet package is in ship package

---
+++ PGCGame/src/com/buildcoolrobots/games/pgcgame/Powerups/BasePowerup.java
@@ -0,0 +1,7 @@
+package com.buildcoolrobots.games.pgcgame.Powerups;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+public class BasePowerup extends Sprite {
+
+}
---
+++ PGCGame/src/com/buildcoolrobots/games/pgcgame/Ships/Bullets/Bullet.java
@@ -0,0 +1,7 @@
+package com.buildcoolrobots.games.pgcgame.Ships.Bullets;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+public class Bullet extends Sprite {
+
+}
