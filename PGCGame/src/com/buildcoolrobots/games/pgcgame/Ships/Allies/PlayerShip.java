3850beac542e007d0297dee17af63d7b616e8aec
elrosso01 <rosst2001@aol.com>
2013-07-30 15:01:59 -0700

    finished playerShip

--- PGCGame/src/com/buildcoolrobots/games/pgcgame/Ships/Allies/PlayerShip.java
+++ PGCGame/src/com/buildcoolrobots/games/pgcgame/Ships/Allies/PlayerShip.java
@@ -1,5 +1,6 @@
 package com.buildcoolrobots.games.pgcgame.Ships.Allies;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 import com.buildcoolrobots.games.pgcgame.Ships.BaseShip;
@@ -8,4 +9,19 @@ public class PlayerShip extends BaseShip {
 	public PlayerShip(Vector2 position, Vector2 speed, Texture texture) {
 		super(position, speed, texture);
 	}
+
+public void Update() {
+
+	if(Gdx.input.getX() < 200){
+		
+		this.setPosition(Gdx.input.getX(),Gdx.input.getY());
+
+	}
+	
+}
+
+
+
+
+
 }
