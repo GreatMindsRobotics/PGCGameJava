ac689428771bba9d15fba23b53ea78c71f0fbb91
Rehaan <rehaanirf@yahoo.com>
2013-07-31 15:06:11 -0700

    Worked on State Manager

--- PGCGame/src/com/buildcoolrobots/games/pgcgame/CoreTypes/StateManager.java
+++ PGCGame/src/com/buildcoolrobots/games/pgcgame/CoreTypes/StateManager.java
@@ -1,12 +1,110 @@
 package com.buildcoolrobots.games.pgcgame.CoreTypes;
 
-public class StateManager {
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Stack;
+import java.util.UUID;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Rectangle;
+import com.buildcoolrobots.games.pgcgame.CoreTypes.Enums.GameLevel;
+import com.buildcoolrobots.games.pgcgame.CoreTypes.Enums.ScreenType;
+import com.buildcoolrobots.games.pgcgame.CoreTypes.Enums.ShipTypes;
+import com.buildcoolrobots.games.pgcgame.Ships.Bullets.Bullet;
+import com.buildcoolrobots.games.pgcgame.Ships.Enemies.BaseEnemyShip;
+
+
+public abstract class StateManager {
 	
+	// Private Fields
+    public static int HealthPacks = 0;
+    private static Stack<ScreenType> _screenStack = new Stack<ScreenType>();
+    private static ScreenType _screenState = ScreenType.TITLESCREEN;
+    private static UUID _enemyID = UUID.randomUUID();
+    private static int _spaceBucks = 200000;
+    
+    // Public Fields
+    public static ArrayList<Bullet> LegitBullets = new ArrayList<Bullet>();
+    public static boolean IsWSFirstUpdate = true;
+    public static final Rectangle WorldSize = new Rectangle(0, 0, (Gdx.graphics.getWidth()) * 5, Gdx.graphics.getHeight());
+    
+    public static ShipTypes SelectedShip;
+    //public static ShipTier SelectedTier;
+
+    public static int SpacePoints = 0;
+    
+    public static final ArrayList<BaseEnemyShip> ActiveShips = new ArrayList<BaseEnemyShip>();
+    
+    public static int lives = 5;
+    
+    private static GameLevel _level = GameLevel.LEVEL1;
+    public static GameLevel CurrentLevel = GameLevel.LEVEL1;
+    public static boolean nextLevel = false;
+    
+    
+    //public static ScreenManager AllScreens;
+    
+    
+    public static final Random RandomGenerator = new Random();
+    
+    //get and sets
+    
+    public static GameLevel getLevel() {
+		return _level;
+    }
+    public static void setLevel(GameLevel level) {
+    	if (level != _level) {
+    		if (CurrentLevel == _level) {
+    			//_level++;
+    			
+    		}
+    		_level = level;
+    		// check if level completed?
+    	}
+    }
+    public static ScreenType getScreenState() {
+    	return _screenState;
+    }
+    public static void setScreenState(ScreenType ScreenState) {
+    	_screenStack.push(ScreenState);
+    	_screenState = ScreenState;
+    	//SwitchScreen(ScreenState); Need to make this method
+    }
+    public static int getSpaceBucks() {
+    	return _spaceBucks;
+    }
+    public static void setSpaceBucks(int SpaceBucks) {
+    	_spaceBucks = SpaceBucks;
+    }
+    public static UUID getEnemyID() {
+    	return _enemyID;
+    }
+    public static void setEnemyID(UUID EnemyID) {
+    	_enemyID = EnemyID;
+    }
+    
+
+    public static void Reset()
+    {
+        //Screens.Shop.firstShop = true;
+        nextLevel = false;
+        _level = GameLevel.LEVEL1;
+        SpacePoints = 0;
+        Stack<ScreenType> _screenStack = new Stack<ScreenType>();
+        _spaceBucks = 200000;
+        IsWSFirstUpdate = true;
+        _enemyID = UUID.randomUUID();
+        lives = 5;
+    }
+    
+    
+    
 	public static class DebugData {
-		
 	}
 	
 	public static class Options {
 		
 	}
+	
+	
 }
