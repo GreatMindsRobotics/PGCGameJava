70b2872ebccc43dc93764bd6da03d8cb3d2f591a
binkers <brandon98@gmail.com>
2013-08-01 10:25:59 -0700

    made scrolling background better
    it goes on forever now

--- PGCGame/src/com/buildcoolrobots/games/pgcgame/Screens/TitleScreen.java
+++ PGCGame/src/com/buildcoolrobots/games/pgcgame/Screens/TitleScreen.java
@@ -16,22 +16,32 @@ import com.buildcoolrobots.games.pgcgame.CoreTypes.Enums.ShipTypes;
 
 public class TitleScreen extends BaseScreen {
 	ExtendedSprite background;
+	ExtendedSprite background2;
 	public TitleScreen(SpriteManager allSprites, SpriteBatch spriteBatch) {
 		super(allSprites, spriteBatch);
 
 		background = new ExtendedSprite(ScreenType.TITLESCREEN.ScreenTexture());
 		background.setPosition(0, 0);
 		background.xSpeed = -0.5f;
-
+		
+		background2 = new ExtendedSprite(ScreenType.TITLESCREEN.ScreenTexture());
+		background2.setPosition(background.getX() + background.getWidth(), 0);
+		background2.xSpeed = -0.5f;
+		
 		allSprites.add(background);
+		allSprites.add(background2);
 	}
 	
 	@Override
 	public void update(float deltaTime) {
 		super.update(deltaTime);
 		
-		if (background.getX() + background.getWidth() <= Gdx.graphics.getWidth()) {
-			background.setPosition(0, 0);
+		if (background.getX() + background.getWidth() <= 0) {
+			background.setPosition(background2.getX() + background2.getWidth(), 0);
+		}
+		
+		if (background2.getX() + background2.getWidth() <= 0) {
+			background2.setPosition(background.getX() + background.getWidth(), 0);
 		}
 	}
 
