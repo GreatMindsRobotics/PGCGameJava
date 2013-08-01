70633e32383ae4ba03af4519e73898ac2c4502d6
binkers <brandon98@gmail.com>
2013-08-01 15:04:27 -0700

    more titlescreen stuff
    changed background texture

--- PGCGame/src/com/buildcoolrobots/games/pgcgame/Screens/TitleScreen.java
+++ PGCGame/src/com/buildcoolrobots/games/pgcgame/Screens/TitleScreen.java
@@ -1,5 +1,7 @@
 package com.buildcoolrobots.games.pgcgame.Screens;
 
+import java.util.Random;
+
 import me.pagekite.glen3b.gjlib.ExtendedLabel;
 import me.pagekite.glen3b.gjlib.ExtendedSprite;
 import me.pagekite.glen3b.gjlib.SpriteManager;
@@ -17,19 +19,23 @@ import com.buildcoolrobots.games.pgcgame.CoreTypes.Enums.ShipTypes;
 
 public class TitleScreen extends BaseScreen {
 	
-	ExtendedSprite background;
-	ExtendedSprite background2;
+	private ExtendedSprite background;
+	private ExtendedSprite background2;
+	private ExtendedSprite ship;
+	
+	Random rand = new Random();
+	
 	public TitleScreen(SpriteManager allSprites, SpriteBatch spriteBatch) {
 		super(allSprites, spriteBatch);
 		
-		StateManager._titleScreenBGSpriteDebug = false;
+		StateManager.TitleScreenBGSpriteDebug = false;
 		
 		background = new ExtendedSprite(ScreenType.TITLESCREEN.ScreenTexture());
 		background.setPosition(0, 0);
 		background.xSpeed = -1f;
 		
 		background2 = new ExtendedSprite(ScreenType.TITLESCREEN.ScreenTexture());
-		if (!StateManager._titleScreenBGSpriteDebug) {
+		if (!StateManager.TitleScreenBGSpriteDebug) {
 			background2.setPosition(background.getX() + background.getWidth(), 0);
 		}
 		else {
@@ -37,16 +43,31 @@ public class TitleScreen extends BaseScreen {
 		}
 		background2.xSpeed = -1f;
 		
+		ship = new ExtendedSprite(ShipTypes.PLAYERSHIP.GameTexture());
+		ship.setPosition(-300, -100);
+		ship.xSpeed = 3;
+		ship.ySpeed = 1;
+		
 		allSprites.add(background);
 		allSprites.add(background2);
+		allSprites.add(ship);
 	}
 	
 	@Override
 	public void update(float deltaTime) {
 		super.update(deltaTime);
 		
+		ship.xSpeed *= 1.018;
+		ship.ySpeed *= 1.018;
+		
+		if (ship.getX() > Gdx.graphics.getWidth()) {
+			ship.setPosition(-100, rand.nextInt(Gdx.graphics.getHeight() - 100));
+			ship.xSpeed = 3;
+			ship.ySpeed = rand.nextInt(5) - 3;
+		}
+		
 		if (background.getX() + background.getWidth() <= 0) {
-			if (!StateManager._titleScreenBGSpriteDebug) {
+			if (!StateManager.TitleScreenBGSpriteDebug) {
 				background.setPosition(background2.getX() + background2.getWidth(), 0);
 			}
 			else {
@@ -55,7 +76,7 @@ public class TitleScreen extends BaseScreen {
 		}
 		
 		if (background2.getX() + background2.getWidth() <= 0) {
-			if (!StateManager._titleScreenBGSpriteDebug) {
+			if (!StateManager.TitleScreenBGSpriteDebug) {
 				background2.setPosition(background.getX() + background.getWidth(), 0);
 			}
 			else {
