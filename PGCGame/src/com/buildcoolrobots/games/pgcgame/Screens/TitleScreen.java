0aba895ce8f20000ac2d2ccbee4aa369b563b2fa
binkers <brandon98@gmail.com>
2013-08-01 11:10:30 -0700

    made debug feature in titlescreen work
    puts 1px line in between backgrounds

--- PGCGame/src/com/buildcoolrobots/games/pgcgame/Screens/TitleScreen.java
+++ PGCGame/src/com/buildcoolrobots/games/pgcgame/Screens/TitleScreen.java
@@ -11,22 +11,31 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
 import com.buildcoolrobots.games.pgcgame.CoreTypes.BaseScreen;
+import com.buildcoolrobots.games.pgcgame.CoreTypes.StateManager;
 import com.buildcoolrobots.games.pgcgame.CoreTypes.Enums.ScreenType;
 import com.buildcoolrobots.games.pgcgame.CoreTypes.Enums.ShipTypes;
 
 public class TitleScreen extends BaseScreen {
+	
 	ExtendedSprite background;
 	ExtendedSprite background2;
 	public TitleScreen(SpriteManager allSprites, SpriteBatch spriteBatch) {
 		super(allSprites, spriteBatch);
-
+		
+		StateManager._titleScreenBGSpriteDebug = false;
+		
 		background = new ExtendedSprite(ScreenType.TITLESCREEN.ScreenTexture());
 		background.setPosition(0, 0);
-		background.xSpeed = -0.5f;
+		background.xSpeed = -1f;
 		
 		background2 = new ExtendedSprite(ScreenType.TITLESCREEN.ScreenTexture());
-		background2.setPosition(background.getX() + background.getWidth(), 0);
-		background2.xSpeed = -0.5f;
+		if (!StateManager._titleScreenBGSpriteDebug) {
+			background2.setPosition(background.getX() + background.getWidth(), 0);
+		}
+		else {
+			background2.setPosition(background.getX() + background.getWidth() + 1, 0);
+		}
+		background2.xSpeed = -1f;
 		
 		allSprites.add(background);
 		allSprites.add(background2);
@@ -37,11 +46,21 @@ public class TitleScreen extends BaseScreen {
 		super.update(deltaTime);
 		
 		if (background.getX() + background.getWidth() <= 0) {
-			background.setPosition(background2.getX() + background2.getWidth(), 0);
+			if (!StateManager._titleScreenBGSpriteDebug) {
+				background.setPosition(background2.getX() + background2.getWidth(), 0);
+			}
+			else {
+				background.setPosition(background2.getX() + background2.getWidth() + 1, 0);
+			}
 		}
 		
 		if (background2.getX() + background2.getWidth() <= 0) {
-			background2.setPosition(background.getX() + background.getWidth(), 0);
+			if (!StateManager._titleScreenBGSpriteDebug) {
+				background2.setPosition(background.getX() + background.getWidth(), 0);
+			}
+			else {
+				background2.setPosition(background.getX() + background.getWidth() + 1, 0);
+			}
 		}
 	}
 
