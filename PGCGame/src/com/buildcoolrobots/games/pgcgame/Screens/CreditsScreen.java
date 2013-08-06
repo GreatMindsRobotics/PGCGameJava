d064b736788a5ea762edb088e58b6bbc6e6a7734
1737ddance <peaceloveyummydance@gmail.com>
2013-08-06 11:22:01 -0700

    PGCGameCore now extends Game class; added Credits

--- PGCGame/src/com/buildcoolrobots/games/pgcgame/Screens/CreditsScreen.java
+++ PGCGame/src/com/buildcoolrobots/games/pgcgame/Screens/CreditsScreen.java
@@ -3,6 +3,7 @@ package com.buildcoolrobots.games.pgcgame.Screens;
 import me.pagekite.glen3b.gjlib.SpriteManager;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.buildcoolrobots.games.pgcgame.CoreTypes.BGSprite;
 import com.buildcoolrobots.games.pgcgame.CoreTypes.BaseScreen;
 import com.buildcoolrobots.games.pgcgame.CoreTypes.Enums.ScreenType;
 
@@ -10,7 +11,8 @@ public class CreditsScreen extends BaseScreen {
 
 	public CreditsScreen(SpriteManager allSprites, SpriteBatch target, ScreenType screenType) {
 		super(allSprites, target, screenType);
-		// TODO Auto-generated constructor stub
+		
+		BGSprite.scrollingBackground(allSprites);
 	}
 
 }
