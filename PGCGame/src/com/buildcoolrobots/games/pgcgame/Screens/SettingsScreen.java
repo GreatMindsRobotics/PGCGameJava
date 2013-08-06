2ff8d23327ce902ed22d9fc7339791caf7638589
binkers <brandon98@gmail.com>
2013-08-06 10:37:41 -0700

    added debug feature for settings screen
    click f3 to get to settingsscreen. also moved scrolling background image
    to gameimage.

--- PGCGame/src/com/buildcoolrobots/games/pgcgame/Screens/SettingsScreen.java
+++ PGCGame/src/com/buildcoolrobots/games/pgcgame/Screens/SettingsScreen.java
@@ -3,6 +3,7 @@ package com.buildcoolrobots.games.pgcgame.Screens;
 import me.pagekite.glen3b.gjlib.SpriteManager;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.buildcoolrobots.games.pgcgame.CoreTypes.BGSprite;
 import com.buildcoolrobots.games.pgcgame.CoreTypes.BaseScreen;
 import com.buildcoolrobots.games.pgcgame.CoreTypes.Enums.ScreenType;
 
@@ -11,6 +12,7 @@ public class SettingsScreen extends BaseScreen {
 	public SettingsScreen(SpriteManager allSprites, SpriteBatch target, ScreenType screenType) {
 		super(allSprites, target, screenType);
 		// TODO Auto-generated constructor stub
+		BGSprite.scrollingBackground(allSprites);
 	}
 
 }
