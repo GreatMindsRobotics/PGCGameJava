9a3b7adcb20fe108f96584e0d3a5205dddab79c1
binkers <brandon98@gmail.com>
2013-08-08 13:53:17 -0700

    made dpad arrows tint grey when touched

--- PGCGame/src/com/buildcoolrobots/games/pgcgame/Screens/GameScreen.java
+++ PGCGame/src/com/buildcoolrobots/games/pgcgame/Screens/GameScreen.java
@@ -3,6 +3,8 @@ package com.buildcoolrobots.games.pgcgame.Screens;
 import me.pagekite.glen3b.gjlib.ExtendedSprite;
 import me.pagekite.glen3b.gjlib.SpriteManager;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.buildcoolrobots.games.pgcgame.CoreTypes.BaseScreen;
 import com.buildcoolrobots.games.pgcgame.CoreTypes.Enums.GameImage;
@@ -28,13 +30,47 @@ public class GameScreen extends BaseScreen {
 		dpadCenter.setPosition(100,100);
 		dpadUp.setPosition(dpadCenter.getX(), dpadCenter.getY() + dpadUp.getHeight());
 		dpadDown.setPosition(dpadCenter.getX(), dpadCenter.getY() - dpadDown.getHeight());
+		dpadLeft.setPosition(dpadCenter.getX() - dpadLeft.getWidth(), dpadCenter.getY());
+		dpadRight.setPosition(dpadCenter.getX() + dpadRight.getWidth(), dpadCenter.getY());
 		
 		allSprites.add(dpadCenter);
 		allSprites.add(dpadUp);
 		allSprites.add(dpadDown);
+		allSprites.add(dpadLeft);
+		allSprites.add(dpadRight);
 	}
+	
+	boolean lastTouch = false;
 	
 	public void update(float deltaTime) {
 		super.update(deltaTime);
+		
+		if (!Gdx.input.isTouched()) {
+			dpadUp.setColor(Color.WHITE);
+			dpadDown.setColor(Color.WHITE);
+			dpadLeft.setColor(Color.WHITE);
+			dpadRight.setColor(Color.WHITE);
+			lastTouch = false;
+		}
+		
+		if (Gdx.input.isTouched()) {
+			if (Gdx.input.getX() >= dpadUp.getX() && Gdx.input.getX() <= dpadUp.getX() + dpadUp.getWidth() &&
+				Gdx.graphics.getHeight() - Gdx.input.getY() >= dpadUp.getY() && Gdx.graphics.getHeight() - Gdx.input.getY() <= dpadUp.getY() + dpadUp.getHeight() && !lastTouch) {
+				dpadUp.setColor(Color.GRAY);
+				lastTouch = true;
+			} else if (Gdx.input.getX() >= dpadDown.getX() && Gdx.input.getX() <= dpadDown.getX() + dpadDown.getWidth() &&
+			Gdx.graphics.getHeight() - Gdx.input.getY() >= dpadDown.getY() && Gdx.graphics.getHeight() - Gdx.input.getY() <= dpadDown.getY() + dpadDown.getHeight() && !lastTouch) {
+				dpadDown.setColor(Color.GRAY);
+				lastTouch = true;
+			} else if (Gdx.input.getX() >= dpadLeft.getX() && Gdx.input.getX() <= dpadLeft.getX() + dpadLeft.getWidth() &&
+			Gdx.graphics.getHeight() - Gdx.input.getY() >= dpadLeft.getY() && Gdx.graphics.getHeight() - Gdx.input.getY() <= dpadLeft.getY() + dpadLeft.getHeight() && !lastTouch) {
+				dpadLeft.setColor(Color.GRAY);
+				lastTouch = true;
+			} else if (Gdx.input.getX() >= dpadRight.getX() && Gdx.input.getX() <= dpadRight.getX() + dpadRight.getWidth() &&
+			Gdx.graphics.getHeight() - Gdx.input.getY() >= dpadRight.getY() && Gdx.graphics.getHeight() - Gdx.input.getY() <= dpadRight.getY() + dpadRight.getHeight() && !lastTouch) {
+				dpadRight.setColor(Color.GRAY);
+				lastTouch = true;
+			}
+		}
 	}
 }
